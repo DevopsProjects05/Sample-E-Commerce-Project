bf32f39116cf10382aad15e9f879d4c448c5c9b1
root <root@ip-172-31-4-203.ap-south-1.compute.internal>
2024-12-07 01:49:28 +0000

    First Push

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example JavaScript to handle the "Add to Cart" functionality
+document.querySelectorAll('.add-to-cart').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Item added to cart!');
+    });
+});
+
