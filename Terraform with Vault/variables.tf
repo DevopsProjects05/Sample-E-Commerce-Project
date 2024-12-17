33768f05a674ef146a73b09b081408472b9fb9bc
Nuthan Gatla <NuthanGatla@gmail>
2024-12-17 20:35:14 +0530

    Tool Image

--- Terraform with Vault/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "vault_token" {
-  default = "s.RjBGvi2oiAB5CZn1ufzPoI8U"
-}
-
-variable "key_pair" {
-  description = "This keyp_pair where we use to connect through SSH"
-  default     = "DevOps_Practice" # Replace with your key pair
-}
