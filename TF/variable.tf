b7ca181f433fbb3048647d162dbe1fc561f3aac7
Nuthan Gatla <NuthanGatla@gmail>
2024-12-26 18:56:30 +0530

    JENKINS WITH SONARQUBE

---
+++ TF/variable.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  default = "AKIARHQBNQH472RZ5DP2"
+}
+
+variable "secret_key" {
+    default = "wAWlCIhhcioeRtrwodgaWBHLAphJKCCq+2YCVnLW"
+  
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "ami" {
+  default = "ami-0fd05997b4dff7aac"
+}
